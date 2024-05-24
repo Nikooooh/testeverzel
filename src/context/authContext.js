9b21081cb5cb5d4f18d75fab9e3f2b25f92b28a4
Nikooooh <nicolastrevis@hotmail.com>
2024-05-24 14:28:40 -0300

    ✨ Feat: Add auth context

---
+++ src/context/authContext.js
@@ -0,0 +1,31 @@
+import React, { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const navigate = useNavigate();
+
+  const login = (email, password) => {
+    if (email === "admin@hotmail.com" && password === "admin") {
+      setIsAdmin(true);
+      navigate("/adicionar-veiculos");
+    } else {
+      alert("Credenciais inválidas");
+    }
+  };
+
+  const logout = () => {
+    setIsAdmin(false);
+    navigate("/login");
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAdmin, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
