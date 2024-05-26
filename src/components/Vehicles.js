7f81e117e1e4f4936999ca2e3b09b063ed1061ce
Nikooooh <nicolastrevis@hotmail.com>
2024-05-26 20:05:51 -0300

    ♻️ Refactor: Vehicle forms

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { useAuth } from "../context/authContext";
+import { useNavigate } from "react-router-dom";
 
 const VehicleCatalog = () => {
   const { logout } = useAuth();
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     name: "",
     brand: "",
@@ -33,20 +35,7 @@ const VehicleCatalog = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (
-      !formData.name ||
-      !formData.brand ||
-      !formData.model ||
-      !formData.photo
-    ) {
-      setErrors({
-        name: !formData.name ? "Campo obrigatório" : "",
-        brand: !formData.brand ? "Campo obrigatório" : "",
-        model: !formData.model ? "Campo obrigatório" : "",
-        photo: !formData.photo ? "Campo obrigatório" : "",
-      });
-      return;
-    }
+
     console.log(formData);
     setFormData({
       name: "",
@@ -54,7 +43,7 @@ const VehicleCatalog = () => {
       model: "",
       photo: null,
     });
-    setErrors({});
+    navigate("/");
   };
 
   return (
@@ -72,96 +61,59 @@ const VehicleCatalog = () => {
       </div>
       <form onSubmit={handleSubmit} className="max-w-md mt-20 mx-auto">
         <div className="mb-4">
-          <label
-            htmlFor="name"
-            className="block mb-1 font-semibold text-gray-800"
-          >
-            Nome: <span className="text-red-600">*</span>
+          <label className="block text-sm font-medium text-gray-700">
+            Nome
           </label>
           <input
             type="text"
-            id="name"
             name="name"
             value={formData.name}
             onChange={handleChange}
-            className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
-              errors.name
-                ? "border-red-500"
-                : "border-gray-300 focus:border-blue-500"
-            }`}
+            className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            required
           />
-          {errors.name && <p className="text-red-600">{errors.name}</p>}
         </div>
-
         <div className="mb-4">
-          <label
-            htmlFor="brand"
-            className="block mb-1 font-semibold text-gray-800"
-          >
-            Marca: <span className="text-red-600">*</span>
+          <label className="block text-sm font-medium text-gray-700">
+            Marca
           </label>
           <input
             type="text"
-            id="brand"
             name="brand"
             value={formData.brand}
             onChange={handleChange}
-            className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
-              errors.brand
-                ? "border-red-500"
-                : "border-gray-300 focus:border-blue-500"
-            }`}
+            className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            required
           />
-          {errors.brand && <p className="text-red-600">{errors.brand}</p>}
         </div>
-
         <div className="mb-4">
-          <label
-            htmlFor="model"
-            className="block mb-1 font-semibold text-gray-800"
-          >
-            Modelo: <span className="text-red-600">*</span>
+          <label className="block text-sm font-medium text-gray-700">
+            Modelo
           </label>
           <input
             type="text"
-            id="model"
             name="model"
             value={formData.model}
             onChange={handleChange}
-            className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
-              errors.model
-                ? "border-red-500"
-                : "border-gray-300 focus:border-blue-500"
-            }`}
+            className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            required
           />
-          {errors.model && <p className="text-red-600">{errors.model}</p>}
         </div>
-
         <div className="mb-4">
-          <label
-            htmlFor="photo"
-            className="block mb-1 font-semibold text-gray-800"
-          >
-            Foto: <span className="text-red-600">*</span>
+          <label className="block text-sm font-medium text-gray-700">
+            Foto
           </label>
           <input
             type="file"
-            id="photo"
             name="photo"
-            accept="image/*"
             onChange={handleFileChange}
-            className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
-              errors.photo
-                ? "border-red-500"
-                : "border-gray-300 focus:border-blue-500"
-            }`}
+            className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            required
           />
-          {errors.photo && <p className="text-red-600">{errors.photo}</p>}
         </div>
-
         <button
           type="submit"
-          className="w-full px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700"
+          className="w-full px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700"
         >
           Adicionar Veículo
         </button>
